31351ef1d5702ad59a078fc46cd0fbce594d29c9
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2026-02-28 14:51:31 +0900

    VOCA-9 초기구조설정 (#1)
    Co-authored-by: Younghan-J0 <jo-yh@booklista.co.jp>

---
+++ lib/shared/database/app_database.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'app_database.sqlite'));
+
+    debugPrint('📂 Database path: ${file.path}');
+
+    return NativeDatabase.createInBackground(file, logStatements: kDebugMode);
+  });
+}

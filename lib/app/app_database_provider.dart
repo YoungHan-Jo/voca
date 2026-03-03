c0f6c4141aac8c2ce7eb45530d80dd3e996fa18c
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2026-03-04 00:24:40 +0900

    seeder (#4)
    Co-authored-by: Younghan-J0 <jo-yh@booklista.co.jp>

--- lib/app/app_database_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:voca/shared/database/app_database.dart';
-
-final appDatabaseProvider = Provider<AppDatabase>((ref) => AppDatabase());
